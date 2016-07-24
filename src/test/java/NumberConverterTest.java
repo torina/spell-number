338f5f15eef99918c44d8ecb332fda3bdc34c1a9
torina <eternalove4>
2016-07-24 18:46:29 +0200

    First commit

---
+++ src/test/java/NumberConverterTest.java
@@ -0,0 +1,20 @@
+import task1.NumberConverter;
+
+import java.util.stream.IntStream;
+import java.util.logging.ConsoleHandler;
+
+/**
+ * Created by Administrator on 7/24/2016.
+ */
+public class NumberConverterTest {
+    public void testSingle() {
+
+        IntStream.range(1111111111, 1111111113).forEach(
+                i -> System.out.println(NumberConverter.toText(String.valueOf(i))));
+    }
+
+    public static void main(String[] args) {
+        NumberConverterTest test = new NumberConverterTest();
+        test.testSingle();
+    }
+}
